04054c2332fcee24cc9460e750934027b363d279
Marek Grzymala <marek.grzymala@grandparade.co.uk>
2020-04-03 07:52:53 +0200

    fixed formatting in SPACE_USED_PER_PARTITION_PER_TABLE_NAME.sql

--- SIZE_ROW_COUNTS - PARTITIONS/SPACE_USED_PER_PARTITION_PER_TABLE_NAME.sql
+++ SIZE_ROW_COUNTS - PARTITIONS/SPACE_USED_PER_PARTITION_PER_TABLE_NAME.sql
@@ -11,30 +11,28 @@ SELECT
 	, au.UsedMB
 	, st.name AS [TableName]
 	, c.name AS [ColumnName]
-	, case when pf.boundary_value_on_right = 1 then 'less than' when pf.boundary_value_on_right is null then '' else 'less than or equal to' End as Comparison
+	, CASE WHEN pf.boundary_value_on_right = 1 THEN 'less than' WHEN pf.boundary_value_on_right IS NULL THEN '' ELSE 'less than or equal to' END AS Comparison
 	, rv.value
 FROM 
 	sys.partitions prt
-	inner join sys.indexes ix on ix.object_id = prt.object_id and ix.index_id = prt.index_id
-	
-	INNER JOIN sys.tables st ON prt.object_id = st.object_id
-	INNER JOIN sys.index_columns ic ON (ic.partition_ordinal > 0 AND ic.index_id = ix.index_id AND ic.object_id = st.object_id)
-	INNER JOIN sys.columns c ON (c.object_id = ic.object_id AND c.column_id = ic.column_id)
-	
-	inner join sys.data_spaces ds on ds.data_space_id = ix.data_space_id
-	left join sys.partition_schemes ps on ps.data_space_id = ix.data_space_id
-	left join sys.partition_functions pf on pf.function_id = ps.function_id
-	left join sys.partition_range_values rv on rv.function_id = pf.function_id AND rv.boundary_id = prt.partition_number
-	left join sys.destination_data_spaces dds on dds.partition_scheme_id = ps.data_space_id AND dds.destination_id = prt.partition_number
-	left join sys.filegroups fg on fg.data_space_id = ISNULL(dds.data_space_id,ix.data_space_id) 
-	inner join (
-					select 
-						str(sum(total_pages)*8./1024,10,2) as [TotalMB],str(sum(used_pages)*8./1024,10,2) as [UsedMB]
-						,container_id
-					from sys.allocation_units
-					group by container_id
-				) au
-				on au.container_id = prt.partition_id
+	INNER JOIN  sys.indexes ix                      ON ix.object_id = prt.object_id and ix.index_id = prt.index_id
+	INNER JOIN  sys.tables st                       ON prt.object_id = st.object_id
+	INNER JOIN  sys.index_columns ic                ON (ic.partition_ordinal > 0 AND ic.index_id = ix.index_id AND ic.object_id = st.object_id)
+	INNER JOIN  sys.columns c                       ON (c.object_id = ic.object_id AND c.column_id = ic.column_id)
+	INNER JOIN  sys.data_spaces ds                  ON ds.data_space_id = ix.data_space_id
+	LEFT JOIN   sys.partition_schemes ps            ON ps.data_space_id = ix.data_space_id
+	LEFT JOIN   sys.partition_functions pf          ON pf.function_id = ps.function_id
+	LEFT JOIN   sys.partition_range_values rv       ON rv.function_id = pf.function_id AND rv.boundary_id = prt.partition_number
+	LEFT JOIN   sys.destination_data_spaces dds     ON dds.partition_scheme_id = ps.data_space_id AND dds.destination_id = prt.partition_number
+	LEFT JOIN   sys.filegroups fg                   ON fg.data_space_id = ISNULL(dds.data_space_id,ix.data_space_id) 
+	INNER JOIN (
+					SELECT 
+						        str(sum(total_pages)*8./1024,10,2) as [TotalMB],str(sum(used_pages)*8./1024,10,2) as [UsedMB]
+						        ,container_id
+					FROM        sys.allocation_units
+					GROUP BY    container_id
+				)   au
+				ON  au.container_id = prt.partition_id
 
 WHERE st.name IN ('YourTableName')
 ORDER BY ix.type_desc;
