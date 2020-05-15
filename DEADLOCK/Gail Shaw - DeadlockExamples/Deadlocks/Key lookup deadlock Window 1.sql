96e5e14cebea880959b830b17168de833acd7c5d
Marek Grzymala <marek.grzymala@grandparade.co.uk>
2020-05-15 17:31:42 +0200

    added Gail Shaw - DeadlockExamples

---
+++ DEADLOCK/Gail Shaw - DeadlockExamples/Deadlocks/Key lookup deadlock Window 1.sql
@@ -0,0 +1,12 @@
+USE AdventureWorks2017
+GO
+
+
+SET LOCK_TIMEOUT 300000 -- 5 minutes
+
+-- basic key-lookup deadlock window 1
+
+--Run this then switch to window 2 and run that code
+
+SELECT * FROM [Production].[Product] WITH (INDEX=[AK_Product_ProductNumber]) WHERE ProductNumber LIKE 'H%'
+GO 100
